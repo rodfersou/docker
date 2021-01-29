4251048d630118086bfe3359f845523d4d4c13cc
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2021-01-29 12:41:39 +0700

    Review docker volume

--- base/dotfiles/base/oh-my-zsh/custom/000_-_nix.zsh
+++ base/dotfiles/base/oh-my-zsh/custom/000_-_nix.zsh
@@ -1,4 +1,4 @@
-trap 'echo got SIGINT' SIGINT
+# trap 'echo got SIGINT' SIGINT
 
 if [[ "$WINDOW" == '1' ]]; then
     if [ ! -d /nix/var ]; then
@@ -11,8 +11,11 @@ if [[ "$WINDOW" == '1' ]]; then
     source $HOME/.nix-profile/etc/profile.d/nix.sh
 
     if [ $BOOTSTRAP_NIX ]; then
+        nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
+        nix-channel --update
         nix-env -i -f $(dirname $0)/000_-_nix.nix
+        read
     fi
 fi
 
-trap SIGINT
+# trap SIGINT
