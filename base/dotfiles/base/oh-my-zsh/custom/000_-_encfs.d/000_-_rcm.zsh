3f1434e0213509189051c378fa303dcf9c766f67
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2021-01-31 14:39:53 +0700

    More fine tunes

--- base/dotfiles/base/oh-my-zsh/custom/000_-_encfs.d/000_-_rcm.zsh
+++ base/dotfiles/base/oh-my-zsh/custom/000_-_encfs.d/000_-_rcm.zsh
@@ -1,3 +1,4 @@
+ln -sf /srv $HOME/.srv
 rcdn
 rcup
 (cd $HOME/.ssh/ids && chmod 600 *)
--- base/dotfiles/base/oh-my-zsh/custom/gitim.zsh
+++ base/dotfiles/base/oh-my-zsh/custom/gitim.zsh
@@ -1,7 +1,5 @@
 if [[ "$WINDOW" == '1' ]]; then
     if [ $(ls /srv | wc -l) = "0"  ]; then
-        (cd /srv && gitim -o rodfersou    -d . --ssh --shallow)
-        (cd /srv && gitim -o boughtbymany -d . --ssh --shallow)
-        (cd /srv && gitim -o slyinfo      -d . --ssh --shallow)
+        gitim -d /srv --ssh --shallow
     fi
 fi
